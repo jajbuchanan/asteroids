bb56c2195995a3f90bb87256a1583e9442a88d3d
jajbuchanan <axtract@gmail.com>
2024-08-24 13:04:51 +0000

    add the pygame functions and initial screen

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+pygame.init()
+screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+black = (0,0,0)
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+    screen.fill((0,0,0))
+    pygame.display.flip()
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
