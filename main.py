13de188b72ef26d05e73950a068aebe512a5925f
ThePrimeagen <the.primeagen@aol.com>
2024-08-24 15:23:56 +0200

    add an infinite game loop

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import pygame
 from constants import *
 
 pygame.init()
-screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-black = (0,0,0)
-while True: 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill((0,0,0))
-    pygame.display.flip()
+
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
